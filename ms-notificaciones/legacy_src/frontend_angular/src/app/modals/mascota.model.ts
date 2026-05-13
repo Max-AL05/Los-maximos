c38ba302a2e4313b3334787df7577b72388bdaa6
Loxsos <129271492+Loxsos@users.noreply.github.com>
2026-05-13 16:45:12 -0600

    Delete ms-notificaciones directory

--- ms-notificaciones/legacy_src/frontend_angular/src/app/modals/mascota.model.ts
+++
@@ -1,9 +0,0 @@
-// src/app/models/mascota.model.ts
-export interface Mascota {
-  id: number;
-  imagenUrl: string;
-  nombre: string;
-  descripcion: string;
-  estado: 'perdida' | 'encontrada' | 'adopcion';
-  // Cualquier otro campo que necesites
-}
